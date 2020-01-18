f445be4a804cf9e6d4b4b158fac943f3218191e4
Kade Walter <kadewalter1@gmaill.com>
2020-01-17 19:30:21 -0600

    created xc project

---
+++ WoW_Heroes/WoW_Heroes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WoW_Heroes
+//
+//  Created by Kade Walter on 1/17/20.
+//  Copyright © 2020 Kade Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
